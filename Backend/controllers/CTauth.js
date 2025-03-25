8c67641715b68c36a83eab03a45742e1d35960d4
GODOFRULES <godofrules666@gmail.com>
2025-03-25 22:47:36 +0700

    server

--- Backend/controllers/CTauth.js
+++ Backend/controllers/CTauth.js
@@ -1,6 +1,16 @@
 exports.register = async(req, res) => {
   //code
   try {
+    const {email,password} = req.body
+    if(!email){
+      return res.status(400).json({message:"email is required"})
+    }
+    if(!password){
+      return res.status(400).json({message:"password is required"})
+    }
+    
+
+    console.log(email,password);
     res.send("hi register in controller!!!");
   } catch (err) {
     console.log(err);
