ff78f19a6e79c46888b28fc57ea693ba92c26222
GODOFRULES <godofrules666@gmail.com>
2025-03-28 03:40:54 +0700

    server

--- Backend/controllers/CTproduct.js
+++ Backend/controllers/CTproduct.js
@@ -1,15 +1,9 @@
-const { console } = require("inspector");
+// const { console } = require("inspector");
 const prisma = require("../config/prisma");
 
 exports.saveProduct = async (req, res) => {
   try {
-    const {
-      qrCode,
-      name,
-      description,
-      supplierId,
-      images,
-    } = req.body;
+    const { qrCode, name, description, supplierId, images } = req.body;
 
     const product = await prisma.product.create({
       data: {
@@ -17,12 +11,17 @@ exports.saveProduct = async (req, res) => {
         name: name,
         description: description,
         supplierId: parseInt(supplierId),
-        images: [],
+        images: {
+          create: images.map((item) => ({
+            idasset: item.idasset,
+            idpublic: item.idpublic,
+            url: item.url,
+            SecureUrl: item.SecureUrl,
+          })),
+        },
       },
     });
-    res.send(product);
-
-    res.send("hi saveProduct");
+    res.send("Save Product Success");
   } catch (err) {
     console.log(err);
     res
@@ -32,7 +31,16 @@ exports.saveProduct = async (req, res) => {
 };
 exports.listsProduct = async (req, res) => {
   try {
-    res.send("hi listsProduct");
+    const { count } = req.params;
+    const products = await prisma.product.findMany({
+      take: parseInt(count),
+      orderBy: { createdAt: "desc" },
+      include: {
+        supplier: true,
+        images: true,
+      },
+    });
+    res.send(products);
   } catch (err) {
     console.log(err);
     res
@@ -42,7 +50,36 @@ exports.listsProduct = async (req, res) => {
 };
 exports.updateProduct = async (req, res) => {
   try {
-    res.send("hi updateProduct");
+    const { qrCode, name, description, supplierId, images } = req.body;
+
+    //ລົບຮູບເກົາອອກກ່ອນອັບໃຫມ່
+    const { id } = req.params;
+    await prisma.image.deleteMany({
+      where: {
+        productId: Number(id),
+      },
+    });
+
+    await prisma.product.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        qrCode: qrCode,
+        name: name,
+        description: description,
+        supplierId: parseInt(supplierId),
+        images: {
+          create: images.map((item) => ({
+            idasset: item.idasset,
+            idpublic: item.idpublic,
+            url: item.url,
+            SecureUrl: item.SecureUrl,
+          })),
+        },
+      },
+    });
+    res.send("Update Success");
   } catch (err) {
     console.log(err);
     res
@@ -52,7 +89,14 @@ exports.updateProduct = async (req, res) => {
 };
 exports.removeProduct = async (req, res) => {
   try {
-    res.send("hi removeProduct");
+    const { id } = req.params;
+    const products = await prisma.product.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    res.send(products);
   } catch (err) {
     console.log(err);
     res
@@ -62,7 +106,17 @@ exports.removeProduct = async (req, res) => {
 };
 exports.readProduct = async (req, res) => {
   try {
-    res.send("hi readProduct");
+    const { id } = req.params;
+    const products = await prisma.product.findFirst({
+      where: {
+        id: Number(id),
+      },
+      include: {
+        supplier: true,
+        images: true,
+      },
+    });
+    res.send(products);
   } catch (err) {
     console.log(err);
     res
@@ -70,10 +124,84 @@ exports.readProduct = async (req, res) => {
       .json({ message: "Server error readProduct in controller!!!" });
   }
 };
+exports.listbyProduct = async (req, res) => {
+  try {
+    const { sort, order, limit } = req.body;
+    console.log(sort, order, limit);
+    const products = await prisma.product.findMany({
+      take: limit,
+      orderBy: { [sort]: order },
+      include: {
+        supplier: true,
+        images: true,
+      },
+    });
+    res.send(products);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Server error listbyProduct in controller!!!" });
+  }
+};
 
+const handleQuery = async (req, res, query) => {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        name: {
+          contains: query,
+        },
+      },
+      include: {
+        supplier: true,
+        images: true,
+      },
+    });
+    res.send(products);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Server error Search handleQuery in controller!!!" });
+  }
+};
+
+const handleSuppiler = async (req, res, supplierId) => {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        supplierId: {
+          in: supplierId.map((id) => Number(id)),
+        },
+      },
+      include: {
+        supplier: true,
+        images: true,
+      },
+    });
+    res.send(products);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Server error Search handleQuery in controller!!!" });
+  }
+};
 exports.searchfiltersProduct = async (req, res) => {
   try {
-    res.send("hi searchfiltersProduct");
+    const { query, unit, supplier } = req.body;
+    if (query) {
+      console.log("query-->", query);
+      await handleQuery(req, res, query);
+    }
+    // if (unit) {
+    //   console.log("unit-->", unit);
+    // }
+    if (supplier) {
+      console.log("supplier-->", supplier);
+      await handleSuppiler(req, res, supplier);
+    }
   } catch (err) {
     console.log(err);
     res
--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { saveProduct,listsProduct,updateProduct,removeProduct,readProduct,searchfiltersProduct } = require('../controllers/CTproduct')
+const { saveProduct,listsProduct,updateProduct,removeProduct,readProduct,listbyProduct,searchfiltersProduct } = require('../controllers/CTproduct')
 
 router.post('/product',saveProduct)
 router.get('/products/:count',listsProduct)
 router.put('/product/:id',updateProduct)
+router.get('/product/:id',readProduct)
 router.delete('/product/:id',removeProduct)
-router.post('/productby',readProduct)
+router.post('/productby', listbyProduct)
 router.post('/search/filters',searchfiltersProduct)
 
 
